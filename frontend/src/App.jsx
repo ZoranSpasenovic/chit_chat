033b8c621f3a5ecc8a1784be0b37d4347a79d3f3
ZoranSpasenovic <spalespasenovic@gmail.com>
2025-04-08 18:08:52 +0200

    implement routing and add page components: Home, SignUp, Login, Settings, Profile; create RootLayout and NavBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import RootLayout from "./layouts/RootLayout"
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+import SettingsPage from './pages/SettingsPage'
+import ProfilePage from './pages/ProfilePage'
+
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const router = createBrowserRouter([{
+    path: "/", element: <RootLayout />, children: [
+      { index: true, element: <HomePage /> },
+      { path: "signup", element: <SignUpPage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "settings", element: <SettingsPage /> },
+      { path: "profile", element: <ProfilePage /> }
+
+    ]
+  }
+
+  ])
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
