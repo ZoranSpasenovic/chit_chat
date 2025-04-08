755b10f3070eb70a8af262aeb5a2040208884ed9
ZoranSpasenovic <spalespasenovic@gmail.com>
2025-04-08 08:38:47 +0200

    implement message handling: add getMessages and sendMessage functions; update routes for user messages

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,3 +1,60 @@
+const Message = require("../models/messageModel");
 const User = require("../models/userModel");
+const cloudinary = require("../lib/cloudinary");
 
-const getUsersForSideBar = async (req, res) => {};
+const getUsersForSideBar = async (req, res) => {
+  try {
+    const myUserId = req.user._id;
+    const filteredUsers = await User.find({ _id: { $ne: myUserId } });
+
+    res.status(200).json(filteredUsers);
+  } catch (err) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const getMessages = async (req, res) => {
+  try {
+    const receiverId = req.params.id;
+    const senderId = req.user._id;
+    const messages = await Message.find({
+      $or: [
+        { senderId, receiverId },
+        { senderId: receiverId, receiverId: senderId },
+      ],
+    });
+  } catch (err) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const receiverId = req.params.id;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+    await newMessage.save();
+
+    res.status(200).json(newMessage);
+  } catch (err) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+module.exports = {
+  getMessages,
+  getUsersForSideBar,
+  sendMessage,
+};
--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const {
+  getMessages,
+  getUsersForSideBar,
+  sendMessage,
+} = require("../controllers/messageController");
+const protect = require("../middleware/protect");
 
 const router = express.Router();
+
+router.get("/users", protect, getUsersForSideBar);
+
+router.get("/:id", protect, getMessages);
+
+router.post("/send/:id", protect, sendMessage);
 
 module.exports = router;
