0ef8bbca594b145a711b101e810676839570e278
ZoranSpasenovic <spalespasenovic@gmail.com>
2025-04-15 18:56:23 +0200

    createing socket.io server and conneting socket.io-client to server socket.io

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,12 @@ const dotenv = require("dotenv");
 const db = require("./lib/db");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const { io, app, server } = require("./lib/socket");
 
 const authRoutes = require("./routes/authRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 
 dotenv.config();
-const app = express();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json({ limit: "10mb" }));
@@ -21,6 +21,6 @@ app.use("/api/messages", messageRoutes);
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("server is running on port " + PORT);
 });
